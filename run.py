58ddabd8a65c34fe02d306f5ba12db9bf4687467
peterkush <pkungu004@gmail.com>
2020-11-30 03:09:46 +0300

    define bcc to fetch data from bbc

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Index():
-    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
+    newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
     topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
     articles = topheadlines['articles']
@@ -29,3 +29,30 @@ def Index():
     mylist = zip(news, desc, img,url,publAt)
 
     return render_template('index.html', context = mylist)
+
+@app.route('/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+    url = []
+    publAt = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
+        publAt.append(myarticles['publishedAt'])
+
+    mylist = zip(news, desc, img,url,publAt)
+
+
+    return render_template('bbc.html', context = mylist)    
